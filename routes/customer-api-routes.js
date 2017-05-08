bec7d88084fc69c3cce79713070d836944df4780
Esteban lau koo <Esteban lau koo>
2017-05-08 14:38:18 -0400

    customer-api-routes done

---
+++ routes/customer-api-routes.js
@@ -0,0 +1,38 @@
+var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/customers", function(req, res) {
+        // 1. Add a join to include all of each Author's Posts
+        db.Customer.findAll({}).then(function(dbCustomer) {
+            res.json(dbCustomer);
+        });
+    });
+
+    app.get("/api/customers/:id", function(req, res) {
+        // 2; Add a join to include all of the Customers's Burgers here
+        db.Customer.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbCustomer) {
+            res.json(dbCustomer);
+        });
+    });
+
+    app.post("/api/customers", function(req, res) {
+        db.Customer.create(req.body).then(function(dbCustomer) {
+            res.json(dbCustomer);
+        });
+    });
+
+    app.delete("/api/customers/:id", function(req, res) {
+        db.Customer.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbCustomer) {
+            res.json(dbCustomer);
+        });
+    });
+
+};
