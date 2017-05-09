ee031974e62bcbcbc08fc6d78c49e2f96da7caa6
Esteban <Esteban_lau_koo@hotmail.com>
2017-05-09 15:50:08 -0400

    added a js

---
+++ views/js/customer.js
@@ -0,0 +1,11 @@
+/**
+ * Created by esteb on 5/8/2017.
+ */
+$(document).ready(function () {
+    console.log("customer.js working")
+    function getAuthor() {
+        $.get("api/customers", function (data) {
+
+        })
+    }
+})
